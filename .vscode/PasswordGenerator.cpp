cb99c55d38f3d86015c5c30d5c183dbea8aefc4a
Sammy White <sammywhite@pop-os.localdomain>
2021-08-03 18:34:00 -0600

    Beginning of Generator

---
+++ .vscode/PasswordGenerator.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main(){
+    //fill different list with special characters
+    string letterList[26] = {a, b, c, d, e, f, g, h, i, j};
+    return 0;
+}

9d8bd79ef86a216aeb1ce83f258bac11cb3cc700
Sammy White <sammywhite@pop-os.localdomain>
2021-08-04 12:00:53 -0600

    Creation of Choice with Password

--- PasswordGenerator.cpp
+++ PasswordGenerator.cpp
@@ -12,7 +12,8 @@ using namespace std;
     list<char> listOfIntegers({'1','2','3','4','5','6','7','8','9','0'});
     list<char> listOfSpecialCharacters({'!','@','#','$','%','^','&','*'});
 
-string getPassword(int lengthOfPassword){
+string getPassword(bool questionUpperCase, bool questionLowerCase, bool questionIntegers, bool questionSpecialint, int lengthOfPassword){
+    
     int listChoice;
     string password = "";
     int numberIterator = 0;
@@ -41,15 +42,56 @@ string getPassword(int lengthOfPassword){
 }
 int main(){
 
-    
-
     //get length of password
-    
+    char choice;
     int lengthOfPassword;
+    bool questionUpperCase, questionLowerCase, questionIntegers, questionSpecial;
+
     cout << "Enter length of password" << endl;
     cin >> lengthOfPassword;
+
+    cout << "Pick how password will be generated, Enter Y/N" << endl;
+    cout << "Would you like capitalized letters?" << endl;
+    cin >> choice;
     cout << endl;
-    cout << getPassword(lengthOfPassword) << endl;
+    if (choice == 'Y'){
+        questionUpperCase = true;
+    }
+    else{
+        questionUpperCase = false;
+    }
+
+    cout << "Would you like undercase letters?" << endl;
+    cin >> choice;
+    cout << endl;
+    if (choice == 'Y'){
+        questionLowerCase = true;
+    }
+    else{
+        questionLowerCase = false;
+    }
+
+    cout << "Would you like numbers in the password?" << endl;
+    cin >> choice;
+    cout << endl;
+    if(choice == 'Y' || choice == 'y'){
+        questionIntegers = true;
+    }
+    else{
+        questionIntegers = false;
+    }
+
+    cout << "Would you like Special Characters in the password?" << endl;
+    cin >> choice;
+    cout << endl;
+    if(choice == 'Y' || choice == 'y'){
+        questionSpecial = true;
+    }
+    else{
+        questionSpecial = false;
+    }
+
+    cout << getPassword(questionUpperCase, questionLowerCase, questionIntegers, questionSpecial, lengthOfPassword) << endl;
     
  
  

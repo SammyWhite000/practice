c5c72ffc4a6a218ac93c3034fb33b5aff1bcb63f
Sammy White <sammywhite@pop-os.localdomain>
2021-08-06 12:57:54 -0600

    Object and print made

--- UniversityAssignment/main.cpp
+++ UniversityAssignment/main.cpp
@@ -6,15 +6,30 @@ using namespace std;
 
 class HashTable{
 public:
+    enum Status{NeverUsed, Tombstone, Occupied};
     struct Entry{
-
         string Data;
-        enum Status{neverUsed, Tombstone, Occupied};
-        
+        Status status = NeverUsed;
     };
-    static const int Size = 26;
+    
 public: 
+    static const int Size = 26;
     HashTable() = default;
+
+    void Print(){
+        //Prints the status based on the enum value
+        for(int x = 0; x < Size; x++){
+            if(m_Entries[x].status == NeverUsed){
+                cout << "Never Used" << endl;
+            }
+            else if (m_Entries[x].status == Tombstone){
+                cout << "Tombstone" << endl;
+            }
+            else if(m_Entries[x].status == Occupied){
+                cout << "Occupied" << endl;
+            }
+        }
+    }
 private:
     Entry m_Entries[Size];
 };
@@ -24,8 +39,12 @@ int main(){
     //Aapple Aorange Dapple Astrawberry
     // orange strawberry
     string input;
+    HashTable hashtable;
     getline(cin, input);
     stringstream ss(input); //Just puts the input right into string stream to be used later
+
+
+    //Go through the whole sstream, and while it is good, take a token string
     while(ss.good()){
         string token;
         ss >> token;
@@ -38,5 +57,7 @@ int main(){
             
         }
     }
+
+    hashtable.Print();
     return 0;
 }
